bbdf7cd9674afd2b2ef9a5e13584ccc36e07bd6a
Shahriar Shubho <shubha.shahriar@gamil.com>
2021-03-30 17:21:18 +0600

    volunter-server

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bfpqs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  if(err){
+    console.log(err)
+  }
+  const collection = client.db("volunteer-network").collection("events")
+  app.post("/addEventDatabase", (req, res) => {
+    const newEvents = req.body;
+    collection.insertOne(newEvents)
+    .then(result => {
+      console.log('insertedCount ',result.insertedCount)
+      res.send(result.insertedCount > 0)
+    })
+  })
+
+  app.get("/events", (req, res) => {
+    collection.find({})
+    .toArray((err, items) => {
+      
+      res.send(items)
+    })
+  })
+
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(5000)
